dcb585035e49610679bb8a58eb5038b8030bfaa6
minipolloxD <vincenzone1v@libero.it>
2023-10-05 10:05:27 +0100

    modified app.py and gitignore to ignore static folder

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import os
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask import Flask
+from flask import make_response
 
 app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+   app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+   response = make_response(open("./static/test.png","r+b").read())
+   response.headers.set("Content-Type","image/png")
+   return response
